31a3492e4e808b6a7d2b5a10dda08551e9da40d8
shamasrehan <shamasrehang@gmail.com>
2024-05-21 15:41:52 +0500

    fix-std-bug

--- primitives/rpc/src/lib.rs
+++ primitives/rpc/src/lib.rs
@@ -16,12 +16,12 @@
 // limitations under the License.
 
 #![cfg_attr(not(feature = "std"), no_std)]
-#![allow(clippy::unnecessary_mut_passed)]
 #![allow(clippy::too_many_arguments)]
 #![warn(unused_crate_dependencies)]
 
 pub use ethereum::{Log, TransactionV0 as LegacyTransaction, TransactionV2 as Transaction};
 use ethereum_types::{Bloom, H160, H256, U256};
+
 use parity_scale_codec::{Decode, Encode};
 use scale_info::TypeInfo;
 
@@ -82,6 +82,39 @@ impl<B: BlockT, C> RuntimeStorageOverride<B, C> for () {
 }
 
 sp_api::decl_runtime_apis! {
+	
+	#[api_version(4)]
+	pub trait DebugRuntimeApi {
+		#[changed_in(4)]
+		fn trace_transaction(
+			extrinsics: Vec<Block::Extrinsic>,
+			transaction: &LegacyTransaction,
+		) -> Result<(), sp_runtime::DispatchError>;
+
+		fn trace_transaction(
+			extrinsics: Vec<Block::Extrinsic>,
+			transaction: &Transaction,
+		) -> Result<(), sp_runtime::DispatchError>;
+
+		fn trace_block(
+			extrinsics: Vec<Block::Extrinsic>,
+			known_transactions: Vec<H256>,
+		) -> Result<(), sp_runtime::DispatchError>;
+	}
+
+	#[api_version(2)]
+	pub trait TxPoolRuntimeApi {
+		#[changed_in(2)]
+		fn extrinsic_filter(
+			xt_ready: Vec<<Block as BlockT>::Extrinsic>,
+			xt_future: Vec<<Block as BlockT>::Extrinsic>,
+		) -> TxPoolResponseLegacy;
+		fn extrinsic_filter(
+			xt_ready: Vec<<Block as BlockT>::Extrinsic>,
+			xt_future: Vec<<Block as BlockT>::Extrinsic>,
+		) -> TxPoolResponse;
+	}
+	
 	/// API necessary for Ethereum-compatibility layer.
 	#[api_version(5)]
 	pub trait EthereumRuntimeRPCApi {
@@ -248,35 +281,7 @@ sp_api::decl_runtime_apis! {
 		#[changed_in(2)]
 		fn convert_transaction(transaction: ethereum::TransactionV0) -> <Block as BlockT>::Extrinsic;
 	}
-}
 
-sp_api::decl_runtime_apis! {
-	// Api version is virtually 4.
-	//
-	// We realized that even using runtime overrides, using the ApiExt interface reads the api
-	// versions from the state runtime, meaning we cannot just reset the versioning as we see fit.
-	//
-	// In order to be able to use ApiExt as part of the RPC handler logic we need to be always
-	// above the version that exists on chain for this Api, even if this Api is only meant
-	// to be used overridden.
-	#[api_version(4)]
-	pub trait DebugRuntimeApi {
-		#[changed_in(4)]
-		fn trace_transaction(
-			extrinsics: Vec<Block::Extrinsic>,
-			transaction: &LegacyTransaction,
-		) -> Result<(), sp_runtime::DispatchError>;
-
-		fn trace_transaction(
-			extrinsics: Vec<Block::Extrinsic>,
-			transaction: &Transaction,
-		) -> Result<(), sp_runtime::DispatchError>;
-
-		fn trace_block(
-			extrinsics: Vec<Block::Extrinsic>,
-			known_transactions: Vec<H256>,
-		) -> Result<(), sp_runtime::DispatchError>;
-	}
 }
 
 #[derive(Clone, Copy, Eq, PartialEq, Debug, Encode, Decode)]
@@ -322,19 +327,4 @@ pub struct TxPoolResponseLegacy {
 pub struct TxPoolResponse {
 	pub ready: Vec<Transaction>,
 	pub future: Vec<Transaction>,
-}
-
-sp_api::decl_runtime_apis! {
-	#[api_version(2)]
-	pub trait TxPoolRuntimeApi {
-		#[changed_in(2)]
-		fn extrinsic_filter(
-			xt_ready: Vec<<Block as BlockT>::Extrinsic>,
-			xt_future: Vec<<Block as BlockT>::Extrinsic>,
-		) -> TxPoolResponseLegacy;
-		fn extrinsic_filter(
-			xt_ready: Vec<<Block as BlockT>::Extrinsic>,
-			xt_future: Vec<<Block as BlockT>::Extrinsic>,
-		) -> TxPoolResponse;
-	}
 }
